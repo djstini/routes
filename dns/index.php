29403826c615395cde6fd9a953599bf60e8867b9
dennisstinauer <dennis@stinauer.net>
2024-03-06 20:50:21 +0100

    feat(request): trying to return funny header

--- dns/index.php
+++ dns/index.php
@@ -11,8 +11,8 @@
 
 // Check Secret 
 if( ! defined('DNS_SECRET') || DNS_SECRET !== base64_decode($_GET['secret'])){
-   header('HTTP/1.1 404 Not Found');
-   die('NOT AUTHORIZED');
+   http_response_code(418);
+   exit;
 }
 
 $interface_nonagon_dev = Interface_Nonagon_Dev::factory('193.159.141.232', '2003:c1:c7ff:10e:1eed:6fff:fe7a:ff1f');
