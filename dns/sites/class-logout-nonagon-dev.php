b4e343a366bc1a6670110b8788935ed159094564
djstini <dennis@stinauer.net>
2024-03-29 18:38:56 +0100

    feat(dns/logout): added logout dyn dns class

---
+++ dns/sites/class-logout-nonagon-dev.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Config file for the interface.nonagon.dev subdomain.
+ *
+ * @author dennisstinauer<dennis@stinauer.net>
+ * @package routes_dns_sites
+ */
+
+/**
+ * Class to handle the registration of logout.nonagon.dev subdomain.
+ */
+class Logout_Nonagon_Dev extends Subdomain_Handler {
+	/**
+	 * Update Records interface method.
+	 *
+	 * @param array $values associative array of values to update. in this case "ipv4" and "ipv6".
+	 * @return boolean true or false depending on success.
+	 */
+	public static function update_records( $values ) {
+		if ( ! defined( 'DNS_AUTH_API_TOKEN' ) || ! defined( 'DNS_ZONE_ID_NONAGON_DEV' ) ) {
+			return false;
+		}
+
+		$auth_api_token = DNS_AUTH_API_TOKEN;
+		$zone_id        = DNS_ZONE_ID_NONAGON_DEV;
+
+		// Instanciate the Subdomain Handler.
+		$interface = new Logout_Nonagon_Dev( $auth_api_token, $zone_id );
+
+		// update the records.
+		if ( isset( $values['ipv4'] ) && ! empty( $values['ipv4'] ) ) {
+			$ipv4_update = $interface->update_record( $interface->get_record( 'logout', 'A' ), $values['ipv4'] );
+		}
+
+		return $ipv4_update;
+	}
+}
